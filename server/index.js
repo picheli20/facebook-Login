8dfaf34cf70eb05256c86491099ee3d48ec6caf5
Fabio Picheli <contato@fabiopicheli.com>
2017-08-07 22:09:51 +0200

    fixing heroku deploy

--- server/index.js
+++ server/index.js
@@ -5,23 +5,6 @@ const netjet = require('netjet');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Gzip
-app.use(compression());
-
-// Insert preload link headers in HTML responses
-app.use(netjet({
-  cache: {
-    max: 500,
-  },
-}));
-
-// Run the app by serving the static files
-// in the dist directory
-const oneDay = 86400000; // in milliseconds
-app.use(express.static(__dirname + '/../dist', {
-  maxage: oneDay,
-}));
-
 // Start the app by listening on the default
 // Heroku port
 app.listen(port);
