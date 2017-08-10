e2d252dae1c44f3c994c25432bff34717ff82a40
Fabio Picheli <contato@fabiopicheli.com>
2017-08-10 12:36:47 +0200

    fixing heroku build

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const compression = require('compression');
 const path = require('path');
 const express = require('express');
-const netjet = require('netjet');
 const app = express();
 const port = process.env.PORT || 8080;
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(`${__dirname}/../dist/`));
 
 // Start the app by listening on the default
 // Heroku port
 app.listen(port);
 
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
   res.sendFile(path.join(`${__dirname}/../dist/index.html`));
 });
